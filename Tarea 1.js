6acf88ec9f711fe97bde6e671409986b66d3bcbc
Sebastian055 <sebastian.cifuentes055@pascualbravo.edu.co>
2024-09-10 19:11:18 -0500

    Tarea Js Sebastian Cifuentes Moncada

---
+++ Tarea 1.js
@@ -0,0 +1,49 @@
+let nombre = "Sebastian";
+let apellidos = "Cifuentes moncada";
+let pais = "Colombia";
+let ciudad= "Medellin";
+let edad = 17;
+
+
+ const pi = 3.14159;
+ const euler= 2.7182;
+ const fi = 1.618;
+ const gravedad = 9.8;
+ const velocidadluz = 299.792;
+
+
+let comoestas = 0;
+let igualelevado;
+let igualmultiplicacion;
+
+const preguntar = () => "¿Como estas?";
+for (let hola = 0; hola < comoestas; hola++);
+
+const elevaralados = (numero1) => numero1 * numero1;
+switch (numero1) {
+    case 1:
+        igualelevado = numero1 ** 3;
+        break;
+    case 2:
+        igualelevado = numero1 ** 4;
+        break;
+
+        return igualelevado;
+}
+
+const multiplicar = (numero2, numero3) => numero2 * numero3;
+switch (numero2) {
+    case 1:
+        igualmultiplicacion = numero3 * 1;
+    break;
+    case 2:
+        igualmultiplicacion = numero3 * 2;
+    break;
+    case 3:
+        igualmultiplicacion = numero3 * 3;
+    break;
+
+    return igualmultiplicacion;
+}
+
+
